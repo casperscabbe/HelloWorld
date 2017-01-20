c6f2d82d47cfa313b060df1034aa23159e8062fc
casperscabbe <c.ovaldius@live.com>
2017-01-20 13:26:18 +0100

    Done

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProgram myProgram;
+            myProgram = new MyProgram();
+            myProgram.Run();
+        }
+    }
+}
